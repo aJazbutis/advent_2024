b7eaba30869ebdca28f194a134ed0a92ea9603d8
aJazbutis <arvydas.jazbutis@gmail.com>
2024-12-01 13:50:18 +0200

    remove slices from pt2

--- d01/pt02/main.go
+++ d01/pt02/main.go
@@ -5,25 +5,9 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"slices"
 	"strconv"
 	"strings"
 )
-
-func freq(arr []int) map[int]int {
-	ret := make(map[int]int)
-	for i := 0; i < len(arr); {
-		val := arr[i]
-		c := 1
-		i++
-		for i < len(arr) && arr[i] == val {
-			c++
-			i++
-		}
-		ret[val] = c
-	}
-	return ret
-}
 
 func main() {
 	if len(os.Args) != 2 {
@@ -34,8 +18,8 @@ func main() {
 		log.Fatal(err.Error())
 	}
 	defer f.Close()
-	left := []int{}
-	right := []int{}
+	left := make(map[int]int)
+	right := make(map[int]int)
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -45,20 +29,26 @@ func main() {
 		if err != nil {
 			log.Fatal(err.Error())
 		}
-		left = append(left, l)
+		_, exist := left[l]
+		if exist {
+			left[l]++
+		} else {
+			left[l] = 1
+		}
 		r, err := strconv.Atoi(nums[1])
+		_, exist = right[r]
+		if exist {
+			right[r]++
+		} else {
+			right[r] = 1
+		}
 		if err != nil {
 			log.Fatal(err.Error())
 		}
-		right = append(right, r)
 	}
-	slices.Sort(left)
-	slices.Sort(right)
-	freq_l := freq(left)
-	freq_r := freq(right)
 	sum := 0
-	for k, v := range freq_l {
-		sum += freq_r[k] * v * k
+	for k, v := range left {
+		sum += right[k] * v * k
 	}
 	fmt.Println(sum)
 }
